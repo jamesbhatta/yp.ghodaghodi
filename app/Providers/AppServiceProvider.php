289871b0ee215c0409bc4af6f39d008608335e75
manojbhatta <jmsbhatta@gmail.com>
2020-02-04 16:47:46 +0545

    set default index length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-    	// 
+    	//
     }
 
     /**
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Blade::directive('invalid', function($key) {
             $key = str_replace(['\'', '"'], '', $key);
             $errors = session()->get('errors') ?: new \Illuminate\Support\ViewErrorBag;
