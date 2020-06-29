a33fa04128ffcd49ddf2ed981a6981010c0a55a4
manojbhatta <jmsbhatta@gmail.com>
2020-06-29 23:19:48 +0545

    hide developers name

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
 			© {{ now()->format('Y') }} Copyright <a href="http://digitalpaschim.com">Digital Paschim </a>
 		</div>
 		<div class="ml-auto">
-			Developed By <a href="http://manojbhatta.com.np">Manoj Bhatta</a>
+			{{-- Developed By <a href="http://manojbhatta.com.np">Manoj Bhatta</a> --}}
 			Powered by <a href="http://mohrain.com">Mohrain WebSoft Pvt. Ltd.</a>
 		</div>
 	</div>
